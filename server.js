d90dbc234606ac526165ad324389f86f2457a281
Robert Freeman <robertisafreeman@gmail.com>
2019-09-16 20:37:54 -0700

    completed all requirements

--- server.js
+++ server.js
@@ -9,6 +9,27 @@ app.get('/', (req, res, next)=> {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/api/people', (req, res, next)=> {
+  Person.findAll()
+    .then( people => res.send(people))
+    .catch(next)
+});
+
+app.get('/api/places', (req, res, next)=> {
+  Place.findAll()
+    .then( places => res.send(places))
+    .catch(next)
+});
+
+app.get('/api/things', (req, res, next)=> {
+  Thing.findAll()
+    .then( things => res.send(things))
+    .catch(next)
+});
 
 
-app.listen(port, ()=> console.log(`Listening on port ${port}`));
+db.syncAndSeed()
+  .then(() => {
+    app.listen(port, ()=> console.log(`Listening on port ${port}`));
+});
+
